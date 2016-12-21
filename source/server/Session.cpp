d162f42c0e4c1d601073aff664b473d04070c5cc
aoleynichenko <ao2310@yandex.ru>
2016-12-21 18:59:14 +0000

    self in Session removed

--- source/server/Session.cpp
+++ source/server/Session.cpp
@@ -32,9 +32,7 @@ namespace meow {
 
 
 		void Session::do_read_header() {
-			auto self(shared_from_this());
-
-			auto read_header_f = [this, self](boost::system::error_code error_code, std::size_t /*length*/) {
+			auto read_header_f = [this](boost::system::error_code error_code, std::size_t /*length*/) {
 				if (!error_code) {
 					msg_buf_.decode_msg_length();
 					do_read_body();
@@ -52,8 +50,7 @@ namespace meow {
 		}
 
 		void Session::do_read_body() {
-			auto self(shared_from_this());
-			auto read_body_f = [this, self](boost::system::error_code error_code, std::size_t /*length*/) {
+			auto read_body_f = [this](boost::system::error_code error_code, std::size_t /*length*/) {
 				if (!error_code) {
 					Message msg(msg_buf_);
 					room_.deliver(msg);
@@ -72,9 +69,7 @@ namespace meow {
 		}
 
 		void Session::do_write() {
-			auto self(shared_from_this());
-
-			auto write_f = [this, self](boost::system::error_code error_code, std::size_t /*length*/) {
+			auto write_f = [this](boost::system::error_code error_code, std::size_t /*length*/) {
 				if (!error_code) {
 					write_msgs_.pop_front();
 					if (!write_msgs_.empty())
